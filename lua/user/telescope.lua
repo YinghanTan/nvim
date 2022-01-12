40ce1209c0773b64e8c287d43bc9da4fa6b55518
Yinghan Tan <yinghan.yhtan@gmail.com>
2022-01-12 10:05:10 +0800

    feat: add tree sitter

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,15 +3,13 @@ if not status_ok then
   return
 end
 
-telescope.load_extension('media_files')
-
 local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart"},
+    path_display = { "smart" },
 
     mappings = {
       i = {
@@ -75,25 +73,19 @@ telescope.setup {
         ["<PageDown>"] = actions.results_scrolling_down,
 
         ["?"] = actions.which_key,
-      }
-    }
+      },
+    },
   },
   pickers = {
     -- Default configuration for builtin pickers goes here:
     -- picker_name = {
     --   picker_config_key = value,
-    -- ...
+    --   ...
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
   },
   extensions = {
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = {"png", "webp", "jpg", "jpeg"},
-      find_cmd = "rg" -- find command (defaults to `fd`)
-    }
     -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
@@ -101,3 +93,37 @@ telescope.setup {
     -- please take a look at the readme of the extension you want to configure
   },
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--   extensions = {
+--     media_files = {
+--       -- filetypes whitelist
+--       -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+--       filetypes = {"png", "webp", "jpg", "jpeg"},
+--       find_cmd = "rg" -- find command (defaults to `fd`)
+--     }
+--     -- Your extension configuration goes here:
+--     -- extension_name = {
+--     --   extension_config_key = value,
+--     -- }
+--     -- please take a look at the readme of the extension you want to configure
+--   },
+-- }
