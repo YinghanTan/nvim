3d8fbb5627913fcc981275fb7e4104418c591d9e
yinghan <yinghan.yhtan@gmail.com>
2022-04-21 01:18:31 +0800

    feat: added more telescope for finding files

--- lua/_telescope.lua
+++ lua/_telescope.lua
@@ -1,168 +1,166 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	return
+    return
 end
 
-
 local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
 telescope.setup({
-	defaults = {
+        defaults = {
+            -- prompt_prefix = " ",
+            prompt_prefix = "   ",
+            selection_caret = " ",
+            path_display = { "smart" },
+            initial_mode = "insert",
+            sorting_strategy = "ascending",
+            layout_config = {
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8,
+                },
+                vertical = {
+                    mirror = false,
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120,
+            },
 
-		-- prompt_prefix = " ",
-    prompt_prefix = "   ",
-		selection_caret = " ",
-		path_display = { "smart" },
+            mappings = {
+                i = {
+                    ["<C-n>"] = actions.cycle_history_next,
+                    ["<C-p>"] = actions.cycle_history_prev,
 
-    initial_mode = "insert",
-    sorting_strategy = "ascending",
-    layout_config = {
-       horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-          results_width = 0.8,
-       },
-       vertical = {
-          mirror = false,
-       },
-       width = 0.87,
-       height = 0.80,
-       preview_cutoff = 120,
-    },
+                    ["<C-j>"] = actions.move_selection_next,
+                    ["<C-k>"] = actions.move_selection_previous,
 
-		mappings = {
-			i = {
-				["<C-n>"] = actions.cycle_history_next,
-				["<C-p>"] = actions.cycle_history_prev,
+                    ["<C-c>"] = actions.close,
 
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
+                    ["<Down>"] = actions.move_selection_next,
+                    ["<Up>"] = actions.move_selection_previous,
 
-				["<C-c>"] = actions.close,
+                    ["<CR>"] = actions.select_default,
+                    ["<C-x>"] = actions.select_horizontal,
+                    ["<C-v>"] = actions.select_vertical,
+                    ["<C-t>"] = actions.select_tab,
 
-				["<Down>"] = actions.move_selection_next,
-				["<Up>"] = actions.move_selection_previous,
+                    ["<C-u>"] = actions.preview_scrolling_up,
+                    ["<C-d>"] = actions.preview_scrolling_down,
 
-				["<CR>"] = actions.select_default,
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
-				["<C-t>"] = actions.select_tab,
+                    ["<PageUp>"] = actions.results_scrolling_up,
+                    ["<PageDown>"] = actions.results_scrolling_down,
 
-				["<C-u>"] = actions.preview_scrolling_up,
-				["<C-d>"] = actions.preview_scrolling_down,
+                    ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                    ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                    ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
+                    ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    ["<C-l>"] = actions.complete_tag,
+                    ["<C-_>"] = actions.which_key,
+                    ["<C-/>"] = action_layout.toggle_preview,
+                },
 
-				["<PageUp>"] = actions.results_scrolling_up,
-				["<PageDown>"] = actions.results_scrolling_down,
+                n = {
+                    ["<esc>"] = actions.close,
+                    ["<CR>"] = actions.select_default,
+                    ["<C-x>"] = actions.select_horizontal,
+                    ["<C-v>"] = actions.select_vertical,
+                    ["<C-t>"] = actions.select_tab,
 
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-				["<C-l>"] = actions.complete_tag,
-				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-                -- ["<C-/>"] = action_layout.toggle_preview,
-			},
+                    ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                    ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                    ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
+                    ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
-			n = {
-				["<esc>"] = actions.close,
-				["<CR>"] = actions.select_default,
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
-				["<C-t>"] = actions.select_tab,
+                    ["j"] = actions.move_selection_next,
+                    ["k"] = actions.move_selection_previous,
+                    ["H"] = actions.move_to_top,
+                    ["M"] = actions.move_to_middle,
+                    ["L"] = actions.move_to_bottom,
 
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    ["<Down>"] = actions.move_selection_next,
+                    ["<Up>"] = actions.move_selection_previous,
+                    ["gg"] = actions.move_to_top,
+                    ["G"] = actions.move_to_bottom,
 
-				["j"] = actions.move_selection_next,
-				["k"] = actions.move_selection_previous,
-				["H"] = actions.move_to_top,
-				["M"] = actions.move_to_middle,
-				["L"] = actions.move_to_bottom,
+                    ["<C-u>"] = actions.preview_scrolling_up,
+                    ["<C-d>"] = actions.preview_scrolling_down,
 
-				["<Down>"] = actions.move_selection_next,
-				["<Up>"] = actions.move_selection_previous,
-				["gg"] = actions.move_to_top,
-				["G"] = actions.move_to_bottom,
+                    ["<PageUp>"] = actions.results_scrolling_up,
+                    ["<PageDown>"] = actions.results_scrolling_down,
 
-				["<C-u>"] = actions.preview_scrolling_up,
-				["<C-d>"] = actions.preview_scrolling_down,
-
-				["<PageUp>"] = actions.results_scrolling_up,
-				["<PageDown>"] = actions.results_scrolling_down,
-
-				["?"] = actions.which_key,
-                -- ["<C-/>"] = action_layout.toggle_preview,
-			},
-		},
-	},
-	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
-		--
-		media_files = {
-			-- filetypes whitelist
-			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-			filetypes = { "png", "webp", "jpg", "jpeg" },
-			find_cmd = "rg", -- find command (defaults to `fd`)
-		},
-		file_browser = {
-			-- theme = "ivy",
-			-- require("telescope.themes").get_dropdown {
-			--   previewer = false,
-			--   -- even more opts
-			-- },
-			mappings = {
-				["i"] = {
-					-- your custom insert mode mappings
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-				},
-			},
-		},
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown({
-				previewer = false,
-				-- even more opts
-			}),
-		},
-		fzy_native = {
-			override_generic_sorter = false,
-			override_file_sorter = true,
-		},
-		bookmarks = {
-			-- Available: 'brave', 'buku', 'chrome', 'edge', 'safari', 'firefox'
-			selected_browser = "chrome",
-			-- Either provide a shell command to open the URL
-			url_open_command = "open",
-			-- Or provide the plugin name which is already installed
-			-- Available: 'vim_external', 'open_browser'
-			url_open_plugin = nil,
-			-- Show the full path to the bookmark instead of just the bookmark name
-			full_path = true,
-			-- Provide a custom profile name for Firefox
-			firefox_profile_name = nil,
-		},
-
-        tele_tabby = {
-            use_highlighter = true,
+                    ["?"] = actions.which_key,
+                    ["<C-/>"] = action_layout.toggle_preview,
+                },
+            },
         },
+        pickers = {
+            -- Default configuration for builtin pickers goes here:
+            -- picker_name = {
+            --   picker_config_key = value,
+            --   ...
+            -- }
+            -- Now the picker_config_key will be applied every time you call this
+            -- builtin picker
+        },
+        extensions = {
+            -- Your extension configuration goes here:
+            -- extension_name = {
+            --   extension_config_key = value,
+            -- }
+            -- please take a look at the readme of the extension you want to configure
+            --
+            media_files = {
+                -- filetypes whitelist
+                -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+                filetypes = { "png", "webp", "jpg", "jpeg" },
+                find_cmd = "rg", -- find command (defaults to `fd`)
+            },
+            file_browser = {
+                -- theme = "ivy",
+                -- require("telescope.themes").get_dropdown {
+                --   previewer = false,
+                --   -- even more opts
+                -- },
+                mappings = {
+                    ["i"] = {
+                        -- your custom insert mode mappings
+                    },
+                    ["n"] = {
+                        -- your custom normal mode mappings
+                    },
+                },
+            },
+            ["ui-select"] = {
+                require("telescope.themes").get_dropdown({
+                        previewer = false,
+                        -- even more opts
+                    }),
+            },
+            fzy_native = {
+                override_generic_sorter = false,
+                override_file_sorter = true,
+            },
+            bookmarks = {
+                -- Available: 'brave', 'buku', 'chrome', 'edge', 'safari', 'firefox'
+                selected_browser = "chrome",
+                -- Either provide a shell command to open the URL
+                url_open_command = "open",
+                -- Or provide the plugin name which is already installed
+                -- Available: 'vim_external', 'open_browser'
+                url_open_plugin = nil,
+                -- Show the full path to the bookmark instead of just the bookmark name
+                full_path = true,
+                -- Provide a custom profile name for Firefox
+                firefox_profile_name = nil,
+            },
 
-	},
-})
+            tele_tabby = {
+                use_highlighter = true,
+            },
+
+        },
+    })
 
 
 -- 🔭 Extensions --
