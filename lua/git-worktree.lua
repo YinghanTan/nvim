3058d8ddc1f09694ee79fa2628fe7e84e28d8d5e
yinghan <yinghan.yhtan@gmail.com>
2022-10-09 10:21:27 +0800

    update git work-tree config

--- lua/git-worktree.lua
+++ lua/git-worktree.lua
@@ -1,9 +1,9 @@
 require("git-worktree").setup({
-    change_directory_command = <str> -- default: "cd",
-    update_on_change = <boolean> -- default: true,
-    update_on_change_command = <str> -- default: "e .",
-    clearjumps_on_change = <boolean> -- default: true,
-    autopush = <boolean> -- default: false,
+    change_directory_command = "cd", -- default: "cd",
+    update_on_change = true, -- default: true,
+    update_on_change_command = "e .", -- default: "e .",
+    clearjumps_on_change = true, -- default: true,
+    autopush = false -- default: false,
 })
 
 -- Creates a worktree.  Requires the path, branch name, and the upstream
