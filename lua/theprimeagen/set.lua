734c9b1e78ac7926476d8d8eeee651631a4a5ac6
yinghan <yinghan.yhtan@gmail.com>
2022-12-20 09:55:30 +0800

    update

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,23 +1,23 @@
-vim.opt.guicursor = ""
+-- vim.opt.guicursor = "" -- set fat cursor
 vim.opt.nu = true
 vim.opt.relativenumber = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.smartinde
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
-vim.opt.
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8 -- TODO: add to .vimrc and to neovimrc in vimscript
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.g.mapleader = "\\"
