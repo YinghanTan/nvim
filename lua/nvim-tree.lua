e71bee18f55360bb8db412be85bf1cebe2166372
yinghan <yinghan.yhtan@gmail.com>
2023-03-18 21:20:28 +0800

    feat: add project-nvim

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -13,6 +13,7 @@ nvim_tree.setup({
   hijack_netrw = false,
   hijack_cursor = true,
   sync_root_with_cwd = true,
+  respect_buf_cwd = true,
   update_focused_file = {
     enable = true,
     update_root = true,
