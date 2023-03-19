17e3d4fb05b86cc4d11a2238b370f4c69b0c1158
yinghan <yinghan.yhtan@gmail.com>
2023-03-19 12:40:31 +0800

    fix: multiopen for telescope

--- lua/_telescope.lua
+++ lua/_telescope.lua
@@ -7,31 +7,8 @@ local actions = require("telescope.actions")
 local action_layout = require("telescope.actions.layout")
 local icons = require("icons")
 local action_state = require('telescope.actions.state')
-
--- Enables multi select but will overwrite quickfix list
-local telescope_custom_actions = {}
-function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
-    local picker = action_state.get_current_picker(prompt_bufnr)
-    local selected_entry = action_state.get_selected_entry()
-    local num_selections = #picker:get_multi_selection()
-    if not num_selections or num_selections <= 1 then
-        actions.add_selection(prompt_bufnr)
-    end
-    actions.send_selected_to_qflist(prompt_bufnr)
-    vim.cmd("cfdo " .. open_cmd)
-end
-function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "vsplit")
-end
-function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "split")
-end
-function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "tabe")
-end
-function telescope_custom_actions.multi_selection_open(prompt_bufnr)
-    telescope_custom_actions._multiopen(prompt_bufnr, "edit")
-end
+local transform_mod = require("telescope.actions.mt").transform_mod
+local multiopen = require('telescope_multiopen')
 
 
 telescope.setup({
@@ -58,54 +35,66 @@ telescope.setup({
         },
         mappings = {
             i = {
-                ["<C-n>"] = actions.cycle_history_next,
-                ["<C-p>"] = actions.cycle_history_prev,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-c>"] = actions.close,
-                ["<Down>"] = actions.move_selection_next,
-                ["<Up>"] = actions.move_selection_previous,
-                ["<CR>"] = telescope_custom_actions.multi_selection_open,
-                ["<C-v>"] = telescope_custom_actions.multi_selection_open_vsplit,
-                ["<C-x>"] = telescope_custom_actions.multi_selection_open_split,
-                ["<C-t>"] = telescope_custom_actions.multi_selection_open_tab,
-                ["<C-u>"] = actions.preview_scrolling_up,
-                ["<C-d>"] = actions.preview_scrolling_down,
-                ["<PageUp>"] = actions.results_scrolling_up,
+                ["<C-n>"]      = actions.cycle_history_next,
+                -- ["<CR>"] = actions.select_default,
+                ["<C-p>"]      = actions.cycle_history_prev,
+                ["<C-j>"]      = actions.move_selection_next,
+                ["<C-k>"]      = actions.move_selection_previous,
+                ["<C-c>"]      = actions.close,
+                ["<Down>"]     = actions.move_selection_next,
+                ["<Up>"]       = actions.move_selection_previous,
+                -- ["<C-v>"] = telescope_custom_actions.multi_selection_open_vsplit,
+                -- ["<C-x>"] = telescope_custom_actions.multi_selection_open_split,
+                -- ["<C-t>"] = telescope_custom_actions.multi_selection_open_tab,
+
+                ['<C-v>']      = multiopen.i['<C-v>'],
+                ['<C-x>']      = multiopen.i['<C-s>'],
+                ['<C-t>']      = multiopen.i['<C-t>'],
+                ['<CR>']       = multiopen.i['<CR>'],
+
+                ["<C-u>"]      = actions.preview_scrolling_up,
+                ["<C-d>"]      = actions.preview_scrolling_down,
+                ["<PageUp>"]   = actions.results_scrolling_up,
                 ["<PageDown>"] = actions.results_scrolling_down,
-                ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-                ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-                ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                ["<C-l>"] = actions.complete_tag,
-                ["<M-w>"] = actions.which_key,
-                ["<C-_>"] = action_layout.toggle_preview, -- <C-/> to trigger
+                ["<Tab>"]      = actions.toggle_selection + actions.move_selection_worse,
+                ["<S-Tab>"]    = actions.toggle_selection + actions.move_selection_better,
+                ["<M-q>"]      = actions.send_to_qflist + actions.open_qflist,
+                ["<C-q>"]      = actions.send_selected_to_qflist + actions.open_qflist,
+                ["<C-l>"]      = actions.complete_tag,
+                ["<M-w>"]      = actions.which_key,
+                ["<C-_>"]      = action_layout.toggle_preview, -- <C-/> to trigger
             },
             n = {
-                ["<esc>"] = actions.close,
-                ["<CR>"] = telescope_custom_actions.multi_selection_open,
-                ["<C-v>"] = telescope_custom_actions.multi_selection_open_vsplit,
-                ["<C-x>"] = telescope_custom_actions.multi_selection_open_split,
-                ["<C-t>"] = telescope_custom_actions.multi_selection_open_tab,
-                ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-                ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-                ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                ["j"] = actions.move_selection_next,
-                ["k"] = actions.move_selection_previous,
-                ["H"] = actions.move_to_top,
-                ["M"] = actions.move_to_middle,
-                ["L"] = actions.move_to_bottom,
-                ["<Down>"] = actions.move_selection_next,
-                ["<Up>"] = actions.move_selection_previous,
-                ["gg"] = actions.move_to_top,
-                ["G"] = actions.move_to_bottom,
-                ["<C-u>"] = actions.preview_scrolling_up,
-                ["<C-d>"] = actions.preview_scrolling_down,
-                ["<PageUp>"] = actions.results_scrolling_up,
+                ["<esc>"]      = actions.close,
+                -- ["<CR>"] = actions.select_default,
+                -- ["<C-v>"] = telescope_custom_actions.multi_selection_open_vsplit,
+                -- ["<C-x>"] = telescope_custom_actions.multi_selection_open_split,
+                -- ["<C-t>"] = telescope_custom_actions.multi_selection_open_tab,
+
+                ['<C-v>']      = multiopen.i['<C-v>'],
+                ['<C-x>']      = multiopen.i['<C-s>'],
+                ['<C-t>']      = multiopen.i['<C-t>'],
+                ['<CR>']       = multiopen.i['<CR>'],
+
+                ["<Tab>"]      = actions.toggle_selection + actions.move_selection_worse,
+                ["<S-Tab>"]    = actions.toggle_selection + actions.move_selection_better,
+                ["<M-q>"]      = actions.send_to_qflist + actions.open_qflist,
+                ["<C-q>"]      = actions.send_selected_to_qflist + actions.open_qflist,
+                ["j"]          = actions.move_selection_next,
+                ["k"]          = actions.move_selection_previous,
+                ["H"]          = actions.move_to_top,
+                ["M"]          = actions.move_to_middle,
+                ["L"]          = actions.move_to_bottom,
+                ["<Down>"]     = actions.move_selection_next,
+                ["<Up>"]       = actions.move_selection_previous,
+                ["gg"]         = actions.move_to_top,
+                ["G"]          = actions.move_to_bottom,
+                ["<C-u>"]      = actions.preview_scrolling_up,
+                ["<C-d>"]      = actions.preview_scrolling_down,
+                ["<PageUp>"]   = actions.results_scrolling_up,
                 ["<PageDown>"] = actions.results_scrolling_down,
-                ["<M-w>"] = actions.which_key,
-                ["<C-_>"] = action_layout.toggle_preview, -- <C-/> to trigger
+                ["<M-w>"]      = actions.which_key,
+                ["<C-_>"]      = action_layout.toggle_preview, -- <C-/> to trigger
             },
         },
     },
