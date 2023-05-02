227e8bdbcd99764cc29ab0311e2111e549b7b19e
yinghan <yinghan.yhtan@gmail.com>
2023-05-02 15:26:12 +0800

    update

--- lua/null-ls.lua
+++ lua/null-ls.lua
@@ -54,4 +54,27 @@ null_ls.setup({
     save_after_format = false,
     sources = sources,
     root_dir = null_ls_utils.root_pattern ".git",
+
+    -- -- format on save, synchronous formatting
+    -- -- you can reuse a shared lspconfig on_attach callback here
+    -- on_attach = function(client, bufnr)
+    --     if client.supports_method("textDocument/formatting") then
+    --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --         vim.api.nvim_create_autocmd("BufWritePre", {
+    --             group = augroup,
+    --             buffer = bufnr,
+    --             callback = function()
+    --                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+    --                 -- vim.lsp.buf.formatting_sync()
+    --                 vim.lsp.buf.format({
+    --                     bufnr = bufnr,
+    --                     filter = function(client)
+    --                         return client.name == "null-ls"
+    --                     end,
+    --                 })
+    --             end,
+    --         })
+    --     end
+    -- end,
+
 })
