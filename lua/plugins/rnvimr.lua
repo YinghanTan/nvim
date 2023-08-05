b38cea491d9ae910e4740c1c7fd72e0a1f969ee0
yinghan <yinghan.yhtan@gmail.com>
2023-08-05 21:54:35 +0800

    update

--- lua/plugins/rnvimr.lua
+++ lua/plugins/rnvimr.lua
@@ -3,8 +3,10 @@ return {
     "kevinhwang91/rnvimr",
     cmd = "RnvimrToggle",
     keys = {
-      { "<M-i>", "<C-\\><C-n>:RnvimrResize<CR>", desc = "resize" },
-      { "<leader>tr", ":RnvimrToggle<CR>", desc = "toggle" },
+      { "<M-i>", "<C-\\><C-n>:RnvimrResize 1,8,9,11,5<CR>", mode={"t"}, desc = "resize default" },
+      { "<M-l>", "<C-\\><C-n>:RnvimrResize<CR>", mode={"t"}, desc = "resize special" },
+      { "<leader>tr", ":RnvimrToggle<CR>", mode={"n"}, desc = "toggle" },
+      { "<leader>tr", "<C-\\><C-n>:RnvimrToggle<CR>", mode={"t"}, desc = "toggle" },
     },
 
     init = function()
@@ -16,6 +18,7 @@ return {
       vim.g.rnvimr_enable_picker = 1
       vim.g.rnvimr_draw_border = 0
       vim.g.rnvimr_pick_enable = 1
+      vim.g.rnvimr_bw_enable = 1
       -- Make Neovim wipe the buffers corresponding to the files deleted by Ranger
       vim.g.rnvimr_enable_bw = 1
       -- Add a shadow window, value is equal to 100 will disable shadow
@@ -31,24 +34,8 @@ return {
         yw = "EmitRangerCwd",
       }
 
-      vim.cmd([[
-
-      " Link CursorLine into RnvimrNormal highlight in the Floating window
-      highlight link RnvimrNormal CursorLine
-
-      nnoremap <silent> \tr :RnvimrToggle<CR>
-      tnoremap <silent> \tr <C-\><C-n>:RnvimrToggle<CR>
-
-      " Resize floating window by all preset layouts
-      tnoremap <silent> <M-i> <C-\><C-n>:RnvimrResize<CR>
-
-      " Resize floating window by special preset layouts
-      tnoremap <silent> <M-l> <C-\><C-n>:RnvimrResize 1,8,9,11,5<CR>
-
-      " Resize floating window by single preset layout
-      tnoremap <silent> <M-y> <C-\><C-n>:RnvimrResize 6<CR>
-
-      ]])
+      -- Link CursorLine into RnvimrNormal highlight in the Floating window
+      vim.cmd([[ highlight link RnvimrNormal CursorLine ]])
     end,
   },
 }
