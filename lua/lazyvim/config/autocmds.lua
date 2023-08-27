ce884f939d25a86974c9a3f270e69f5bf424d46f
yinghan <yinghan.yhtan@gmail.com>
2023-08-27 16:44:55 +0800

    update

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -18,14 +18,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- -- resize splits if window got resized
--- vim.api.nvim_create_autocmd({ "VimResized" }, {
---   group = augroup("resize_splits"),
---   callback = function()
---     vim.cmd("tabdo wincmd =")
---   end,
--- })
-
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup("last_loc"),
