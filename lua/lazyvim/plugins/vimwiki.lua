b3c099da6ae60fe4e3a2a4d6edcf339b73ce4714
yinghan <yinghan.yhtan@gmail.com>
2024-01-04 00:37:24 +0800

    update

--- lua/lazyvim/plugins/vimwiki.lua
+++ lua/lazyvim/plugins/vimwiki.lua
@@ -11,6 +11,7 @@ return {
       --- Vim Wiki & Instant Markdown Plugin ---
       vim.g.vimwiki_conceallevel = 0
       vim.g.vimwiki_listsyms = " x"
+      vim.g.vimwiki_global_ext = 0 -- This option will treat all markdown files in your system as part of vimwiki
       vim.g.vimwiki_ext2syntax = {
         [".md"] = "markdown",
         [".markdown"] = "markdown",
@@ -20,9 +21,32 @@ return {
         path = "~/vimwiki/",
         syntax = "markdown",
         ext = ".md",
+        nested_syntaxes = {
+          ["python"] = "python",
+          ["js"]= "js",
+          ["html"]= "html",
+          ["css"]= "css",
+          ["json"]= "json",
+          ["c++"]= "cpp"
+        },
+        diary_rel_path = "inbox/"
+      }
+      local wiki_2 = {
+        path = "~/notes/",
+        syntax = "markdown",
+        ext = ".md",
+        nested_syntaxes = {
+          ["python"] = "python",
+          ["js"]= "js",
+          ["html"]= "html",
+          ["css"]= "css",
+          ["json"]= "json",
+          ["c++"]= "cpp"
+        },
+        diary_rel_path = "inbox/"
       }
 
-      vim.g.vimwiki_list = { wiki_1 }
+      vim.g.vimwiki_list = { wiki_1, wiki_2 }
       vim.g.vimwiki_folding = "expr"
       vim.g.vimwiki_global_ext = 1 -- make sure vimwiki doesn't own all .md files
     end,
