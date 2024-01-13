a1a025514fb0d73564c3ed5c13f71cdaba05496d
yinghan <yinghan.yhtan@gmail.com>
2024-01-13 19:33:56 +0800

    update

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -226,27 +226,3 @@ endfunction
 nnoremap <silent> zj :<c-u>call RepeatCmd('call NextClosedFold("j")')<cr>
 nnoremap <silent> zk :<c-u>call RepeatCmd('call NextClosedFold("k")')<cr>
 ]])
-
-
-vim.cmd([[
-let g:inkscape_graphs_dir = "./assets/"
-function! ink#Ink(image)
-	if getcwd() !~ expand("%:p:h")
-		cd %:p:h
-	endif
-	if !isdirectory(g:inkscape_graphs_dir)
-	    call mkdir(g:inkscape_graphs_dir, "p")
-	endif
-	let b:inline = '![' . a:image . ']('. g:inkscape_graphs_dir . a:image . '.svg)\'
-	call append(line('.'),b:inline)
-	normal jo
-	if filereadable(expand("~/.config/inkscape/templates/default.svg"))
-		exe ":!cp ~/.config/inkscape/templates/default.svg" g:inkscape_graphs_dir . a:image . ".svg"
-	elseif filereadable("/usr/share/inkscape/templates/default.svg")
-		exe ":!cp /usr/share/inkscape/templates/default.svg" g:inkscape_graphs_dir . a:image . ".svg"
-	else 
-		finish
-	endif
-	exe ":!inkscape" g:inkscape_graphs_dir . a:image . ".svg"
-endfunction
-]])
