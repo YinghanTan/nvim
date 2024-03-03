bd1fe01af98f078ae051eccb029e36e7655962f6
yinghan <yinghan.yhtan@gmail.com>
2024-03-03 14:01:31 +0800

    update

--- lua/user/trouble.lua
+++ lua/user/trouble.lua
@@ -5,8 +5,8 @@ local M = {
   keys = {
     { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
     { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-    { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-    { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+    { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "LList to (Trouble)" },
+    { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "QList to (Trouble)" },
     {
       "[q",
       function()
