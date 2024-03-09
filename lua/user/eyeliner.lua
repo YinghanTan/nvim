1fc17b10e539fe9a21fea4523de1f1b8c8b7b7ab
yinghan <yinghan.yhtan@gmail.com>
2024-03-09 18:05:22 +0800

    update

--- lua/user/eyeliner.lua
+++ lua/user/eyeliner.lua
@@ -5,10 +5,15 @@ local M = {
 
 
 function M.config()
+
+  vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#98971a', bold = true, underline = true })
+  vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#fb4934', bold = true, underline = true })
+
   require("eyeliner").setup {
     highlight_on_key = true,
-    dim = true,
+    dim = true
   }
+
 end
 
 return M
