37840f186f9c5b70cf369ecda85d12287c8e6ca1
yinghan <yinghan.yhtan@gmail.com>
2024-03-16 14:12:46 +0800

    update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -181,17 +181,6 @@ function M.config()
           ['[K'] = '@class.outer',
         },
       },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-
-
     },
 
     query_linter = {
