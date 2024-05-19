dd3ea75f12f31984e9751d431f5310f61dd990b2
yinghan <yinghan.yhtan@gmail.com>
2024-05-20 00:55:58 +0800

    update

--- lua/yinghan/plugins/indent-blankline.lua
+++ lua/yinghan/plugins/indent-blankline.lua
@@ -1,10 +1,42 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  branch = "master",
-  commit = "29be0919b91fb59eca9e90690d76014233392bef",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    indent = { char = "┊" },
-  },
+  event = "VeryLazy",
+  commit = "9637670896b68805430e2f72cf5d16be5b97a22a",
+  config = function()
+    vim.keymap.set("n", "yog", "<cmd>IndentBlanklineToggle<cr>", { desc = "toggle indent guide" })
+
+    local icons = require("yinghan.icons")
+    require("indent_blankline").setup({
+      buftype_exclude = { "terminal", "nofile" },
+      filetype_exclude = {
+        "help",
+        "startify",
+        "dashboard",
+        "lazy",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+        "text",
+      },
+      -- char = icons.ui.LineLeft,
+      char = icons.ui.LineMiddle,
+      -- context_char = icons.ui.LineLeft,
+      context_char = icons.ui.LineMiddle,
+      show_trailing_blankline_indent = false,
+      show_first_indent_level = true,
+      use_treesitter = true,
+      show_current_context = true,
+    })
+  end,
 }
+
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   main = "ibl",
+--   branch = "master",
+--   commit = "29be0919b91fb59eca9e90690d76014233392bef",
+--   event = { "BufReadPre", "BufNewFile" },
+--   opts = {
+--     indent = { char = "┊" },
+--   },
+-- }
