723339072e5dfb99f10954c3847b5f47d261d115
yinghan <yinghan.yhtan@gmail.com>
2024-05-19 22:17:52 +0800

    update

---
+++ lua/yinghan/plugins/git-gitlinker.lua
@@ -0,0 +1,16 @@
+return {
+  "linrongbin16/gitlinker.nvim",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
+  event = "VeryLazy",
+  config = function()
+    local wk = require("which-key")
+    wk.register {
+      ["<leader>ggy"] = { "<cmd>GitLink!<cr>", "Git link" },
+      ["<leader>ggY"] = { "<cmd>GitLink! blame<cr>", "Git link blame" },
+    }
+    require("gitlinker").setup({
+      message = false,
+      console_log = false,
+    })
+  end,
+}
