35aaeb261c533c15e291f04a4583592b3fc57ca7
yinghan <yinghan.yhtan@gmail.com>
2024-05-21 16:32:31 +0800

    update

--- lua/yinghan/plugins/navbuddy.lua
+++ lua/yinghan/plugins/navbuddy.lua
@@ -1,20 +1,8 @@
 return {
   "SmiteshP/nvim-navbuddy",
-  branch = "master",
-  commit = "f137a3466a6cd1965cdcc5398daff54e66eebbe5",
   dependencies = {
-    -- {
-    --   "neovim/nvim-lspconfig",
-    --   commit = "d5b6d4366dfd7a1071b930defd365e6d0be258de"
-    -- },
-    {
-      "SmiteshP/nvim-navic",
-      commit = "8649f694d3e76ee10c19255dece6411c29206a54"
-    },
-    {
-      "MunifTanjim/nui.nvim",
-      commit = "c3c7fd618dcb5a89e443a2e1033e7d11fdb0596b"
-    },
+    "SmiteshP/nvim-navic",
+    "MunifTanjim/nui.nvim",
   },
   config = function()
     local wk = require("which-key")
@@ -24,7 +12,13 @@ return {
 
     local navbuddy = require "nvim-navbuddy"
     -- local actions = require("nvim-navbuddy.actions")
-    navbuddy.setup({})
+    navbuddy.setup {
+      window = {
+        border = "rounded",
+      },
+      icons = require("yinghan.library.icons").kind,
+      lsp = { auto_attach = true },
+    }
 
     local opts = { noremap = true, silent = true }
     local keymap = vim.api.nvim_set_keymap
