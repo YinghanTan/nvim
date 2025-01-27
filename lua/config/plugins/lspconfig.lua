43d963e335fdecbb0f70966a7f7bec55b7b0b5d8
yinghan <yinghan.yhtan@gmail.com>
2025-01-27 14:17:38 +0800

    Revert "update"
    This reverts commit bd75204c93e1d79ac17ccbe7d1659e633cd49c64.

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -185,7 +185,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        ts_ls = {
+        tsserver = {
           single_file_support = true
         },
         jsonls = {},
