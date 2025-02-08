4a939f8d5af145996f6814606f00e1954e03f515
yinghan <yinghan.yhtan@gmail.com>
2025-02-08 19:33:59 +0800

    update

--- lua/config/plugins/vim-test.lua
+++ lua/config/plugins/vim-test.lua
@@ -7,7 +7,7 @@ return {
     vim.keymap.set("n", "<leader>t.", ":TestNearest<CR>", {desc = "[t]est [.]line"})
     vim.keymap.set("n", "<leader>t%", ":TestFile<CR>", {desc = "[t]est [%]file"})
     vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", {desc = "[t]est [a]ll"})
-    vim.keymap.set("n", "<leader>tk", ":TestClass<CR>", {desc = "[t]est [k]class"})
+    vim.keymap.set("n", "<leader>tc", ":TestClass<CR>", {desc = "[t]est [c]lass"})
     vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {desc = "[t]est [l]ast"})
     vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", {desc = "[t]est [v]isit"})
     vim.cmd("let test#strategy = 'vimux'")
