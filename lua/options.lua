2f5cb0ab3cf223ada2c2458b44d52de3daf482ad
yinghan <yinghan.yhtan@gmail.com>
2025-04-12 14:42:09 +0800

    update

--- lua/options.lua
+++ lua/options.lua
@@ -227,3 +227,31 @@ if vim.loop.fs_stat(python_path) then
 else
   print("Python3 executable not found at " .. python_path)
 end
+
+-- Set Icons
+-- Debugging
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+vim.fn.sign_define("DapBreakpoint", {text=" ", texthl="DapBreakpoint", linehl="DapBreakpoint", numhl="DapBreakpoint"})
+vim.fn.sign_define("DapBreakpointCondition", {text="󰀘 ", texthl="DapBreakpoint", linehl="DapBreakpoint", numhl="DapBreakpoint"})
+vim.fn.sign_define("DapLogPoint", {text=" ", texthl="DapLogPoint", linehl="DapLogPoint", numhl="DapLogPoint"})
+vim.fn.sign_define("DapStopped", {text=" ", texthl="DapStopped", linehl="DapStopped", numhl="DapStopped"})
+vim.fn.sign_define("DapBreakpointRejected", {text="  ", texthl="DapBreakpoint", linehl="DapBreakpoint", numhl="DapBreakpoint"})
+-- diagnostics
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = "󰌵 ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+  },
+})
