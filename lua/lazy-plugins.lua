e74ac582de2432d6ec5ee0ab159213ceb14d81b5
yinghan <yinghan.yhtan@gmail.com>
2025-09-09 10:10:08 +0800

    feat: update

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -22,21 +22,6 @@ require("lazy").setup({
   -- modular approach: using `requre 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require("config/plugins/gitsigns"),
-  require("config/plugins/which-key"),
-  require("config/plugins/telescope"),
-  -- require("config/plugins/telescope-tabs"),
-  require("config/plugins/lspconfig"),
-  require("config/plugins/django-plus"),
-  require("config/plugins/conform"),
-  require("config/plugins/cmp"),
-  require("config/plugins/gruvbox"),
-  require("config/plugins/todo-comments"),
-  require("config/plugins/treesitter"),
-  require("config/plugins/treesitter-context"),
-  require("config/plugins/ultisnips"),
-  require("config/plugins/honza"),
-
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -46,60 +31,120 @@ require("lazy").setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
 
+  -- git
+  require("config/plugins/gitsigns"),
+  require("config/plugins/vim-fugitive"),
+  require("config/plugins/diffview"),
+
+
+  -- shortcuts
+  require("config/plugins/which-key"),
+  require("config/plugins/vim-eunuch"),
+  require("config/plugins/unimpaired"),
+  require("config/plugins/unimpaired-which-key"),
+  require("config/plugins/vim-togglelist"),
+  require("config/plugins/zoomwintab"),
+
+
+  -- search
+  require("config/plugins/telescope"),
+  -- require("config/plugins/telescope-tabs"),
+  require("config/plugins/fzf"),
+  require("config/plugins/spectre"),
+
+
+  -- language support
+  require("config/plugins/lspconfig"),
+  require("config/plugins/django-plus"),
+  require("config/plugins/treesitter"),
+  require("config/plugins/treesitter-context"),
+  require("config/plugins/ultisnips"),
+  require("config/plugins/honza"),
+  require("config/plugins/conform"),
+  require("config/plugins/cmp"),
+  require("config/plugins/todo-comments"),
+  require("config/plugins/vindent"),
+  require("config/plugins/vim-python-pep8-indent"),
+  require("config/plugins/fidget"),
+  require("config/plugins/jinja"),
+  require("config/plugins/outline"),
+
+
+  -- debug/testing
+  require("config.plugins.lint"),
   require("config.plugins.debug"),
   -- require("config/plugins/vimspector"),
-  require("config.plugins.lint"),
+  require("config/plugins/vim-test"),
+
+
+  -- theme
+  require("config/plugins/gruvbox"),
+  require("config/plugins/nvim-highlight-colors"),
+  require("config/plugins/rainbow-csv"),
+  require("config/plugins/tabline"),
+
+
+  -- file explorer
   require("config.plugins.neo-tree"),
-  require("config/plugins/dial"),
-  require("config/plugins/diffview"),
-  require("config/plugins/fidget"),
-  require("config/plugins/fzf"),
+  require("config/plugins/oil"),
+  require("config/plugins/rnvimr"),
+
+
+  -- navigation
   require("config/plugins/hop"),
   require("config/plugins/navbuddy"),
+
+
+  -- edit
+  require("config/plugins/undotree"),
+  require("config/plugins/nvim-surround"),
+  require("config.plugins.autopairs"),
+  require("config/plugins/dial"),
   require("config/plugins/project"),
+
+
+  -- utilities
+  require("config/plugins/snacks"),
+  require("config/plugins/mini"),
+  require("config/plugins/startuptime"),
+  -- require("config/plugins/tts"),
+  -- require("config/plugins/vim-piper"),
+  require("config/plugins/vimux"),
+
+
+  -- api
   -- require("config/plugins/rest"),
   require("config/plugins/kulala"),
-  require("config/plugins/rnvimr"),
-  require("config/plugins/spectre"),
-  require("config/plugins/startuptime"),
-  require("config/plugins/undotree"),
-  require("config/plugins/vim-eunuch"),
-  require("config/plugins/vim-fugitive"),
-  require("config/plugins/tabline"),
+
+
+  -- fold
+  require("config/plugins/nvim-ufo"),
+  require("config/plugins/pretty-fold"),
+
+
+  -- terminal
+  require("config/plugins/venv-selector"),
   require("config/plugins/vim-slime"),
-  require("config/plugins/vim-togglelist"),
+  require("config/plugins/floaterm"),
+
+
+  -- AI
+  require("config/plugins/avante"),
+
+
+  -- scratchpad
+  require("config/plugins/codi"),
+  require("config/plugins/iron"),
+
+
+  -- Markdown
   require("config/plugins/vimwiki"),
   require("config/plugins/bullets"),
   -- require("config/plugins/wiki"),
-  require("config/plugins/zoomwintab"),
   require("config/plugins/markdown-preview"),
-  require("config/plugins/rainbow-csv"),
-  require("config/plugins/codi"),
-  require("config/plugins/nvim-ufo"),
-  require("config/plugins/pretty-fold"),
-  require("config/plugins/snacks"),
-  require("config/plugins/venv-selector"),
-  require("config/plugins/floaterm"),
-  require("config/plugins/oil"),
-  require("config/plugins/vindent"),
+  require("config/plugins/bullets"),
+  require("config/plugins/img-clip"),
 
-  require("config/plugins/mini"),
-  require("config/plugins/nvim-surround"),
-  require("config.plugins.autopairs"),
-  require("config/plugins/unimpaired"),
-  require("config/plugins/unimpaired-which-key"),
-
-  require("config/plugins/avante"),
-  require("config/plugins/vim-test"),
-  require("config/plugins/jinja"),
-  require("config/plugins/vimux"),
-
-  require("config/plugins/nvim-highlight-colors"),
-  require("config/plugins/vim-python-pep8-indent"),
-  require("config/plugins/iron"),
-  -- require("config/plugins/tts"),
-
-  -- require("config/plugins/vim-piper"),
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
